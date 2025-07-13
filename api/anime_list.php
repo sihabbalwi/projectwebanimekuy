f916cce8bbe7ec9c0aa2c6bbc91f2f797e1d067f
ari5nugroho <ari.nugroho552004@gmail.com>
2025-07-13 18:51:24 +0700

    filter anime_list tipe

--- api/anime_list.php
+++ api/anime_list.php
@@ -4,7 +4,7 @@ include 'koneksi/koneksi.php';
 $limit = 12;
 
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-$page = max($page, 1); 
+$page = max($page, 1);
 
 $offset = ($page - 1) * $limit;
 
@@ -14,15 +14,13 @@ $totalPages = ceil($totalAnime / $limit);
 $search = isset($_GET['search']) ? mysqli_real_escape_string($conn, $_GET['search']) : null;
 
 if ($search) {
-    $query = "SELECT * FROM tb_anime WHERE judul LIKE '%$search%' ORDER BY judul ASC LIMIT $limit OFFSET $offset";
-    $countQuery = "SELECT COUNT(*) as total FROM tb_anime WHERE judul LIKE '%$search%'";
+    $query = "SELECT * FROM tb_anime WHERE tipe='anime' AND judul LIKE '%$search%' ORDER BY judul ASC LIMIT $limit OFFSET $offset";
+    $countQuery = "SELECT COUNT(*) as total FROM tb_anime WHERE tipe='anime' AND judul LIKE '%$search%'";
 } else {
-    $query = "SELECT * FROM tb_anime ORDER BY judul ASC LIMIT $limit OFFSET $offset";
-    $countQuery = "SELECT COUNT(*) as total FROM tb_anime";
+    $query = "SELECT * FROM tb_anime WHERE tipe='anime' ORDER BY judul ASC LIMIT $limit OFFSET $offset";
+    $countQuery = "SELECT COUNT(*) as total FROM tb_anime WHERE tipe='anime'";
 }
 
 $result = mysqli_query($conn, $query);
 $totalAnime = mysqli_fetch_assoc(mysqli_query($conn, $countQuery))['total'];
 $totalPages = ceil($totalAnime / $limit);
-
-?>
